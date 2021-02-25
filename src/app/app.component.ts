37bdc5d58eccc18ce11aad78b7330172f14f9ecb
Tong Bao Loc <loctong>
2021-02-25 22:52:07 +0700

    initital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fb-clone';
+
+  constructor() {
+    console.log('Hello world');
+  }
+}
